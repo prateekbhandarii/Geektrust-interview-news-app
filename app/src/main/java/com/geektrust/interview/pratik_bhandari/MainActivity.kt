faef8e6987ea52acd764ca654f70510054f40c58
Pratik Bhandari <prateek.9770@gmail.com>
2026-01-18 09:28:39 +0530

    Initial commit

---
+++ app/src/main/java/com/geektrust/interview/pratik_bhandari/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.geektrust.interview.pratik_bhandari
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.geektrust.interview.pratik_bhandari.ui.theme.GeektrustinterviewTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GeektrustinterviewTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(
+                       modifier = Modifier.padding(innerPadding)
+                    ) {
+                        
+                    }
+                }
+            }
+        }
+    }
+}
